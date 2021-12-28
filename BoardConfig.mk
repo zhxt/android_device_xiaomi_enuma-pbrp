1298a8289b2e37d07543a18840230b9b916b1dd0
zhxt <xingtao.zhang@yahoo.com>
2021-12-28 23:47:00 +0800

    config for prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,16 +25,34 @@ BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
 # f2fs
 # BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 
+# Crypto
+BOARD_USES_QCOM_FBE_DECRYPTION := true
+
 # A/B
 TW_INCLUDE_REPACKTOOLS := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := 
-#TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+
+# !!!Important: Keep the below line when using prebuilt kernel image in PBRP.
+#  TARGET_KERNEL_SOURCE :=
+# otherwise it will be set to TARGET_KERNEL_SOURCE :=  kernel/${VENDOR}/${DEVICE}
+# by vendor/pb/config/BoardConfigKernel.mk automatically.
+# and with devices that depends on generated_kernel_headers, the two lines bellow are needed:
+#  TARGET_KERNEL_SOURCE := kernel/${VENDOR}/${DEVICE}
+#  TARGET_FORCE_PREBUILT_KERNEL := true
+
+TARGET_FORCE_PREBUILT_KERNEL := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/enuma
 TARGET_KERNEL_CONFIG := vendor/enuma_user_defconfig
-#TARGET_KERNEL_CONFIG := vendor/enuma_defconfig
+
+#BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 134217728
+
+# This is needed to make assert-max-image-size happy.
+BOARD_RECOVERY_BOOTIMAGE_PARTITION_SIZE := 134217728
 
 #Manifest
 # DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
@@ -42,7 +60,6 @@ TARGET_KERNEL_CONFIG := vendor/enuma_user_defconfig
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
-PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
